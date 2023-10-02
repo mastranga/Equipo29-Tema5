5b0ed216d572dbe18fec84d68a61757f4680bbdd
mastranga <brunormastrangelo@gmail.com>
2023-10-02 20:11:04 -0300

    se crea BD sin datos

---
+++ Base/vacunar23-sinData.sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-10-2023 a las 01:08:36
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `vacunar23`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cita`
+--
+
+CREATE TABLE `cita` (
+  `codCita` int(11) NOT NULL,
+  `codRefuerzo` int(11) NOT NULL,
+  `fechaHoraCita` varchar(60) NOT NULL,
+  `centroVacunacion` varchar(60) NOT NULL,
+  `fechaHoraColoca` date NOT NULL,
+  `dni` int(11) NOT NULL,
+  `nroSerie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciudadano`
+--
+
+CREATE TABLE `ciudadano` (
+  `dni` int(11) NOT NULL,
+  `nombreCompleto` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `celular` varchar(60) NOT NULL,
+  `patologia` varchar(60) DEFAULT NULL,
+  `ambitoTrabajo` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `laboratorio`
+--
+
+CREATE TABLE `laboratorio` (
+  `cuit` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `pais` varchar(60) NOT NULL,
+  `domicilio` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vacuna`
+--
+
+CREATE TABLE `vacuna` (
+  `nroSerie` int(11) NOT NULL,
+  `marca` varchar(60) NOT NULL,
+  `medida` double NOT NULL,
+  `fechaCad` date NOT NULL,
+  `estado` tinyint(1) NOT NULL,
+  `cuit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cita`
+--
+ALTER TABLE `cita`
+  ADD PRIMARY KEY (`codCita`),
+  ADD KEY `dni` (`dni`),
+  ADD KEY `nroSerie` (`nroSerie`);
+
+--
+-- Indices de la tabla `ciudadano`
+--
+ALTER TABLE `ciudadano`
+  ADD PRIMARY KEY (`dni`),
+  ADD UNIQUE KEY `email` (`email`,`celular`);
+
+--
+-- Indices de la tabla `laboratorio`
+--
+ALTER TABLE `laboratorio`
+  ADD PRIMARY KEY (`cuit`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `vacuna`
+--
+ALTER TABLE `vacuna`
+  ADD PRIMARY KEY (`nroSerie`),
+  ADD UNIQUE KEY `marca` (`marca`),
+  ADD KEY `cuit` (`cuit`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cita`
+--
+ALTER TABLE `cita`
+  MODIFY `codCita` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `vacuna`
+--
+ALTER TABLE `vacuna`
+  MODIFY `nroSerie` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cita`
+--
+ALTER TABLE `cita`
+  ADD CONSTRAINT `cita_ibfk_1` FOREIGN KEY (`dni`) REFERENCES `ciudadano` (`dni`),
+  ADD CONSTRAINT `cita_ibfk_2` FOREIGN KEY (`nroSerie`) REFERENCES `vacuna` (`nroSerie`);
+
+--
+-- Filtros para la tabla `vacuna`
+--
+ALTER TABLE `vacuna`
+  ADD CONSTRAINT `vacuna_ibfk_1` FOREIGN KEY (`cuit`) REFERENCES `laboratorio` (`cuit`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
